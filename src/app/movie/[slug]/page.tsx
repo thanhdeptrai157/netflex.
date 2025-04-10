76eae8d10282601931d43bd589c5f834dc48150a
Thanh <thanhcap3@gmail.com>
2025-04-10 23:39:22 +0700

    fix: change params inline to interface

--- src/app/movie/[slug]/page.tsx
+++ src/app/movie/[slug]/page.tsx
@@ -1,18 +1,20 @@
 import MovieDetail from '@/components/movie-detail';
 import NewUpdateMovie from '@/components/new-update-movie';
 
-interface Params {
+// yêu cầu params phải tuân theo interface chuẩn PageProps, không viết inline { params: Params }
+interface PageProps {
+  params: {
     slug: string;
+  };
 }
 
-const MovieDetailPage = async ({ params }: { params: Params }) => {
-    return (
-        <div className="flex flex-wrap gap-5 bg-slate-900  h-fit px-3 sm:px-4 lg:px-6 py-20 ">
-            <MovieDetail slug={params.slug} />
-            <NewUpdateMovie />
-        </div>
-    );
+const MovieDetailPage = async ({ params }: PageProps) => {
+  return (
+    <div className="flex flex-wrap gap-5 bg-slate-900 h-fit px-3 sm:px-4 lg:px-6 py-20">
+      <MovieDetail slug={params.slug} />
+      <NewUpdateMovie />
+    </div>
+  );
 };
 
 export default MovieDetailPage;
-
